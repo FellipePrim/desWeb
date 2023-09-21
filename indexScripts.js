392116ab0e3c870fe204f2ed866c2053a12a989b
Fellipe Negherbon Prim <99996732+FellipePrim@users.noreply.github.com>
2023-09-21 17:13:43 -0300

    protótipo do dark mode

--- indexScripts.js
+++ indexScripts.js
@@ -1,1 +1,16 @@
 alert("Seja Bem-Vindo!")
+
+function light(){
+    document.body.style.background='#fff'
+    document.body.style.color='#000'
+    document.a.style.color='#000'
+    document.getElementById("indexLinks").style.color="#000"
+    document.getElementById("indexLinks").style.textDecoration="none"
+}
+function dark(){
+    document.body.style.background='#191919'
+    document.body.style.color='#fff'
+    document.a.style.color='#fff'
+    document.getElementById("indexLinks").style.color="#fff"
+    document.getElementById("indexLinks").style.textDecoration="none"
+}
